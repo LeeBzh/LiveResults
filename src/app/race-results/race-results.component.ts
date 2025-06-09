005a5233f1de50f47ff22e823a7d7ee620a29c06
verticille <155817706+verticille@users.noreply.github.com>
2025-06-09 19:16:04 +0200

    filter race results by runner's category

--- src/app/race-results/race-results.component.ts
+++ src/app/race-results/race-results.component.ts
@@ -1,28 +1,48 @@
 import { Runner } from './../models/runner';
 import { Component, inject } from '@angular/core';
-import { Subscription } from 'rxjs';
 import { RunnerService } from '../runner-service.service';
+import { Category } from '../models/category.enum';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  imports: [CommonModule, FormsModule],
   selector: 'app-race-results',
   templateUrl: './race-results.component.html',
   styleUrls: ['./race-results.component.css']
 })
 export class RaceResultsComponent {
   runners: Runner[] = [];
+  filteredRunners: Runner[] = [];
+  categories = Object.values(Category);
+  selectedCategory: Category | '' = '';
 
   runnerService = inject(RunnerService);
+
 
   ngOnInit(): void {
     this.runnerService.getRunners().subscribe({
       next: (data) => {
         this.runners = data;
+        this.filteredRunners = [...this.runners];
         console.log(this.runners);
       },
       error: (error) => {
         console.error('Error fetching runners:', error);
       }
     });
+  }
+
+  filterRunners() {
+    if (this.selectedCategory === '') {
+      this.filteredRunners = [...this.runners];
+    } else {
+      this.filteredRunners = this.runners.filter(runner => runner.category === this.selectedCategory);
+    }
+  }
+
+  trackByBibNumber(index: number, runner: Runner): number {
+    return runner.bibNumber;
   }
 
 }
